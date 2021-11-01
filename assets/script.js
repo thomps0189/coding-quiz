7d99e93db5f183aabb71af83b80d0d6f400688f6
thomps0189 <angelathompson2011@gmail.com>
2021-10-31 20:09:20 -0500

    assign variables in js

---
+++ assets/script.js
@@ -0,0 +1,4 @@
+// Create a start quiz function that hides the welcome page and shows the questions
+let timeEl = document.querySelector('.timer');
+let welcomeContainerEl = document.querySelector('.welcome-container');
+let questionContainerEl = document.querySelector('.quesetion-container');
